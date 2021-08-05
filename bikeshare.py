eece468444bf34dced7fd999010aec38ef8df855
Avinash Pulugurtha <avinashpulugurtha@gmail.com>
2021-08-05 14:37:19 +0530

    Readability change 3

--- bikeshare.py
+++ bikeshare.py
@@ -117,7 +117,7 @@ def time_stats(df):
     # display the most common day of week
     popular_day_of_week = df['day_of_week'].mode()[0]
 
-    print('Most popular Day Of Week:', popular_day_of_week)
+    print('Most popular day:', popular_day_of_week)
 
     # display the most common start hour
     df['hour']=df['Start Time'].dt.hour
